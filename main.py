9e3dc127540f9fe2aca0d7bb6adcff650975c233
iohnishijima <nishijima.io.hm@tut.jp>
2024-04-15 17:08:40 +0900

    edit main.py, add commit

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ def main():
         #Get serial number 
         serialNumber = methods.parse_response(nreal_light.send_command('3', 'C'))
         print(f"serial number is: {serialNumber}")
+        #Get firmware version
         firmware = methods.parse_response(nreal_light.send_command('3', '5'))
         print(f"firmware is: {firmware}")
-        RGBCamera = methods.parse_response(nreal_light.send_command('1', 'h', '1'))
-        print(f"RGB camera is: {RGBCamera}")
+        #Get Display mode
         displayMode = methods.parse_response(nreal_light.send_command('3', '3'))
         print(f"display mode is: {displayMode}")
+        #Keep RGB camera open
+        RGBCamera = methods.parse_response(nreal_light.send_command('1', 'h', '1'))
+        print(f"RGB camera is: {RGBCamera}")
+        #Check SDK is working
         SDKWorks = methods.parse_response(nreal_light.send_command('@', '3', '1'))
         print(f"SDK works is: {SDKWorks}")
-        # setDisplayMode = methods.parse_response(nreal_light.send_command('1', '3', '3'))
-        # print(f"desplay mode set: {setDisplayMode}")
+        #Set Display mode to 3D
+        setDisplayMode = methods.parse_response(nreal_light.send_command('1', '3', '3'))
+        print(f"desplay mode set: {setDisplayMode}")
+        #parse heartbeat every less than 1 sec
         heartbeat = methods.parse_response(nreal_light.send_command('@', 'K'))
         print(f"heartbeat  is: {heartbeat}")
         
